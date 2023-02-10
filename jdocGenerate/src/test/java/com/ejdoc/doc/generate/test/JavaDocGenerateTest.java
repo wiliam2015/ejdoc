20183fac2de78cca9e3981643a657052b39c3212
wiliam2015 <wiliam.hu@qq.com>
2023-02-10 21:34:33 +0800

    init commit

--- jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/JavaDocGenerateTest.java
+++ jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/JavaDocGenerateTest.java
@@ -9,12 +9,6 @@ public class JavaDocGenerateTest {
     public void printJavaDocTest(){
         JavaDocGenerate javaDocGenerate = new JavaDocGenerate();
         javaDocGenerate.printJavaDoc();
-//
-//        String d="/Users/huhailong1/code/github/hutool/hutool-paraser/docs1/doc";
-//        String d1="/Users/huhailong1/code/github/hutool/hutool-paraser/docs1/doc/projectMetaInfo.json";
-//        String replaceFilePath = StrUtil.replace("", d1, d);
-//        System.out.println("s:"+replaceFilePath);
-//        String replace = d1.replace(d, "");
-//        System.out.println(replace);
+
     }
 }
