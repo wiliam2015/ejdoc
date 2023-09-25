d527a278d5a92815b4fdacab41d78ce11f012f5f
wiliam2015 <wiliam.hu@qq.com>
2023-09-25 09:00:49 +0800

    noNeed

--- jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
+++ jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
@@ -1,24 +1,8 @@
 package com.ejdoc.doc.generate.test;
 
-import com.ejdoc.doc.generate.enums.TemplateTypeEnum;
-import com.ejdoc.doc.generate.model.DocTemplateThemeInfo;
-import com.ejdoc.doc.generate.out.config.DocGenerateConfig;
-import com.ejdoc.doc.generate.out.factory.DocGenerateConfigFactory;
-import com.ejdoc.doc.generate.template.DocTemplateTheme;
-import com.ejdoc.doc.generate.template.markdown.theme.JavaDocDocsifyTemplateTheme;
-
 public class DocsifyThemeTest {
 
     public static void main(String[] args) {
-        DocGenerateConfig markdownConfig = DocGenerateConfigFactory.createMarkdownConfig();
-        DocTemplateTheme docTemplateTheme = new JavaDocDocsifyTemplateTheme(markdownConfig);
-        DocTemplateThemeInfo docTemplateThemeInfo = new DocTemplateThemeInfo();
-        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docmeta/doc");
-        docTemplateThemeInfo.setTemplateType(TemplateTypeEnum.MarkDown);
-        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docMd/doc/markdown");
-        docTemplateThemeInfo.setDocOutRootPath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/doc");
-        docTemplateTheme.writeTemplateThemeFile(docTemplateThemeInfo);
 
-//        ZipUtil.unzip(docTemplateThemeInfo.getRenderFilePath()+"/static.zip",docTemplateThemeInfo.getRenderFilePath());
     }
 }
