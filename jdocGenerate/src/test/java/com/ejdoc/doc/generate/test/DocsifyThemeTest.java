df0390eac88f05ddccb40d8907299aa24cb4f470
wiliam2015 <wiliam.hu@qq.com>
2023-12-28 13:59:01 +0800

    功能优化

--- jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
+++ jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
@@ -6,19 +6,22 @@ import com.ejdoc.doc.generate.out.config.DocGenerateConfig;
 import com.ejdoc.doc.generate.out.factory.DocGenerateConfigFactory;
 import com.ejdoc.doc.generate.template.DocTemplateTheme;
 import com.ejdoc.doc.generate.template.markdown.theme.JavaDocDocsifyTemplateTheme;
+import com.ejdoc.metainfo.seralize.index.MetaIndexContextBuilder;
 
 public class DocsifyThemeTest {
 
     public static void main(String[] args) {
+        MetaIndexContextBuilder.instance().createMetaIndexContext("/Users/huhailong1/code/github/hutool/docmeta/doc");
         DocGenerateConfig markdownConfig = DocGenerateConfigFactory.createMarkdownConfig();
         DocTemplateTheme docTemplateTheme = new JavaDocDocsifyTemplateTheme(markdownConfig);
         DocTemplateThemeInfo docTemplateThemeInfo = new DocTemplateThemeInfo();
-        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docmeta/doc");
+//        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docmeta/doc");
+        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/hutool/docmeta/doc");
         docTemplateThemeInfo.setTemplateType(TemplateTypeEnum.MarkDown);
-        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docMd/doc/markdown");
-        docTemplateThemeInfo.setDocOutRootPath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/doc");
+//        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docMd/doc/markdown");
+        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/hutool/docMd/doc/markdown");
+//        docTemplateThemeInfo.setDocOutRootPath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/doc");
         docTemplateTheme.writeTemplateThemeFile(docTemplateThemeInfo);
 
-//        ZipUtil.unzip(docTemplateThemeInfo.getRenderFilePath()+"/static.zip",docTemplateThemeInfo.getRenderFilePath());
     }
 }
