fcecb636b49a2f5604e2d3558cf130ceb43cf91b
wiliam2015 <wiliam.hu@qq.com>
2024-03-20 08:32:18 +0800

    noNeed

--- jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
+++ jdocGenerate/src/test/java/com/ejdoc/doc/generate/test/DocsifyThemeTest.java
@@ -4,17 +4,6 @@ package com.ejdoc.doc.generate.test;
 public class DocsifyThemeTest {
 
     public static void main(String[] args) {
-        MetaIndexContextBuilder.instance().createMetaIndexContext("/Users/huhailong1/code/github/hutool/docmeta/doc");
-        DocGenerateConfig markdownConfig = DocGenerateConfigFactory.createMarkdownConfig();
-        DocTemplateTheme docTemplateTheme = new JavaDocDocsifyTemplateTheme(markdownConfig);
-        DocTemplateThemeInfo docTemplateThemeInfo = new DocTemplateThemeInfo();
-//        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docmeta/doc");
-        docTemplateThemeInfo.setJsonFilePath("/Users/huhailong1/code/github/hutool/docmeta/doc");
-        docTemplateThemeInfo.setTemplateType(TemplateTypeEnum.MarkDown);
-//        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/docMd/doc/markdown");
-        docTemplateThemeInfo.setRenderFilePath("/Users/huhailong1/code/github/hutool/docMd/doc/markdown");
-//        docTemplateThemeInfo.setDocOutRootPath("/Users/huhailong1/code/github/java/JDKSourceCode1.8/doc");
-        docTemplateTheme.writeTemplateThemeFile(docTemplateThemeInfo);
 
     }
 }
